769391ff7b58e95617bd54b34ea4e04f4fe3df5e
iamKarthik47 <63862140+iamKarthik47@users.noreply.github.com>
2024-02-15 11:46:27 +0530

    completed

---
+++ exp1cube/cube.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class cube
+{
+
+	public static void main(String[] args)
+	{
+	  System.out.println("Enter the limit : ");
+	  Scanner sc=new Scanner(System.in);
+          int num=sc.nextInt();
+          for(int i=1;i<=num;i++)
+            {
+               System.out.println(i+"  "+"cube is"+":"+i*i*i);
+	    }
+         }
+}
