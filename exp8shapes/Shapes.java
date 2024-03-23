83e343fcccb485482b6383d97336d77c3d500492
iamKarthik47 <63862140+iamKarthik47@users.noreply.github.com>
2024-03-23 12:10:05 +0530

    completed

---
+++ exp8shapes/Shapes.java
@@ -0,0 +1,36 @@
+class Shapes{
+
+     class Area{
+            
+            
+             int area(int a)
+                 {
+                  return a*a;
+                  }
+                  
+             int area(int l,int b)
+                 {
+                   return l*b;
+                   }
+                   
+              int  area(int l,int b,int h)
+                  {
+                     return ((2*l*b)+(2*l*h)+(2*b*h));
+                    }
+              
+              }
+              public static void main(String[] args)
+              {
+               int ar;
+               Shapes s=new Shapes();
+               Shapes.Area a=s.new Area();
+               ar=a.area(5);
+               System.out.println("Area of a square : "+ar);
+               ar=a.area(4,3);
+               System.out.println("Area of a square : "+ar);
+ 	       ar=a.area(4,3,5);  
+ 	       System.out.println("Area of a Cuboid : "+ar);             
+               }
+               
+               }
+               
